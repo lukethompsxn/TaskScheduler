fde371206579f879d69e98e43a4962bd89574a40
lukethompson <lukethompsxn@gmail.com>
2018-07-23 17:47:55 +1200

    Begin defining project structure: Creation of config class with command line argument parameter fields

---
+++ src/main/java/se306/a1/scheduler/algorithm/Config.java
@@ -0,0 +1,9 @@
+package se306.a1.scheduler.algorithm;
+
+public class Config {
+    public static String input;
+    public static int numProcessors;
+    public static Integer numCores;
+    public static Boolean hasVisualisation;
+    public static String output;
+}
