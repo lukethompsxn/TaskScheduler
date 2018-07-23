cfa9e5f986a5e64b2f18d08247a7cf92d54934de
Abhinav Behal <abhinav.behal@hotmail.com>
2018-07-23 16:38:54 +1200

    Renamed the package and made the jar runnable

--- src/main/java/se306/a1/scheduler/Main.java
+++ src/main/java/se306/a1/scheduler/Main.java
@@ -1,8 +1,7 @@
-package se306.a1.taskscheduler;
+package se306.a1.scheduler;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello, world");
     }
 }
-
