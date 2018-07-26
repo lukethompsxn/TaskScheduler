6737a4415e13903a719777e78dacc53f5e96165e
Joel <31730723+joelz1@users.noreply.github.com>
2018-07-26 13:05:00 +1200

    Small changes to how the options are selected and parsed. Still need to refactor the class into different package.

--- src/main/java/se306/a1/scheduler/Input/CLIParser.java
+++ src/main/java/se306/a1/scheduler/Input/CLIParser.java
@@ -1,6 +1,7 @@
 package se306.a1.scheduler.Input;
 
 import org.apache.commons.cli.*;
+import java.util.Scanner;
 
 /**
  * This class deals with the command line inputs.
@@ -8,10 +9,17 @@ import org.apache.commons.cli.*;
  * and formats command line inputs.
  * @author Joel Clarke
  **/
-public class ParseCLI {
+public class CLIParser {
 
     private Options options = new Options();
     private HelpFormatter help = new HelpFormatter();
+
+    private String inputFileName;
+    private int numProcessors = 1;
+    private int numCores;
+    private boolean isParallel;
+    private boolean doVisualise;
+    private String outPutFile;
 
     /**
      * Main method which carries out the parsing of the command line inputs.
@@ -22,8 +30,6 @@ public class ParseCLI {
      * @throws ParseException
      */
     public void parseCLI(String[] args) throws ParseException {
-        //Options options = new Options();
-        //HelpFormatter help = new HelpFormatter();
 
         // Calls method which creates the CLI options
         addOptions();
@@ -34,37 +40,62 @@ public class ParseCLI {
         // Checks if help is wanted to be displayed
         if(cmd.hasOption("h") || (cmd.hasOption("help"))) {
             printHelp();
+            System.exit(1);
         }
 
         // Checks if parallel option is set
         if (cmd.hasOption("p")) {
             // set parametrised variable to true
             System.out.println("Has specified parallel option...");
+
+            isParallel = true;
+            String stringCores = cmd.getOptionValue("p");
+
+            // Need to check is valid int
+            if (isInteger(stringCores, 10)) {
+                int cores = Integer.parseInt(cmd.getArgs()[1]);
+                System.out.println(cores);
+                numCores = cores;
+            } else {
+                // p value is not and integer
+                System.out.println("P is not an integer");
+                printHelp();
+                System.exit(1);
+            }
         }
 
         // Checks if if visualization wants to be displayed
         if(cmd.hasOption("v")) {
             // set visualise variable to true
             System.out.println("Has visualise option...");
+            doVisualise = true;
         }
 
         // Checks for file name, and there is only two unnamed CLI (file name + num processors)
-        if (cmd.getArgs().length > 2) {
+        if (cmd.getArgs().length > 2) { // Has too many unnamed inputs
             // Too many unnamed args
             System.out.println("Unknown unnamed inputs found...");
             printHelp();
-        } else if (cmd.getArgs().length == 2)  {
-            String potentialFile = cmd.getArgs()[0];
-            System.out.println(potentialFile);
+            System.exit(1);
+        } else if (cmd.getArgs().length == 2)  { // Has correct number of unnamed inputs
+            inputFileName = cmd.getArgs()[0];
+            System.out.println(inputFileName);
 
             // Need to check 2nd unnamed input is integer
-        } else {
-            // Does not have required args
+            if (isInteger(cmd.getArgs()[1], 10)) {
+                numProcessors = Integer.parseInt(cmd.getArgs()[1]);
+                System.out.println(numProcessors);
+            } else {
+                // P is not and integer
+                System.out.println("P is not an integer");
+                printHelp();
+                System.exit(1);
+            }
+        } else { // Does not have required unnamed inputs
             System.out.println("Required inputs not found...");
             printHelp();
+            System.exit(1);
         }
-
-        //help.printHelp("CLI-Test", "Header for CLI test: ", options, "Footer for CLI test...");
     }
 
     /**
@@ -73,15 +104,6 @@ public class ParseCLI {
      * another Apache Commons CLI object which holds the Option objects.
      */
     private void addOptions() {
-        OptionGroup group = new OptionGroup();
-
-//        Option file = Option.builder("f")
-//                .argName("INPUT.dot")
-//                .desc("A task graph with integer weights in dot format.")
-//                .hasArg(true)
-//                .numberOfArgs(1)
-//                .required(true)
-//                .build();
 
         // Create selection of cores options
         Option cores = Option.builder("p")
@@ -110,12 +132,7 @@ public class ParseCLI {
 
         // Creates option for printing help
         options.addOption(Option.builder("h").longOpt("help").desc("Displays usage of Java scheduler app.").build());
-
-        //group.addOption(file).addOption(cores).addOption(visualise).addOption(outputFile);
-        //group.addOption(cores).addOption(visualise).addOption(outputFile);
         options.addOption(outputFile).addOption(visualise).addOption(cores);
-
-        options.addOptionGroup(group);
     }
 
     /**
@@ -131,4 +148,21 @@ public class ParseCLI {
                 options, "Footer for CLI test...");
     }
 
+
+    /**
+     * This is a helper method which takes in a input of type String and returns
+     * true or false based on whether or not it is an integer. The method also
+     * takes a second input radix, this is what base of numbering system should
+     * be referenced.
+     * @param potentialNum
+     * @param radix
+     * @return boolean
+     */
+    private static boolean isInteger(String potentialNum, int radix) {
+        Scanner sc = new Scanner(potentialNum.trim());
+        if(!sc.hasNextInt(radix)) return false;
+        // Know it starts with a valid int, check rest
+        sc.nextInt(radix);
+        return !sc.hasNext();
+    }
 }
