c850f1b210ec79fefd2d3a7903b8b9bf4f9aa57d
Abhinav Behal <abhinav.behal@hotmail.com>
2018-07-26 19:56:25 +1200

    Implemented the graph parsing functionality, waiting for the TaskGraph class to be defined

--- src/main/java/se306/a1/scheduler/Main.java
+++ src/main/java/se306/a1/scheduler/Main.java
@@ -1,7 +1,17 @@
 package se306.a1.scheduler;
 
+import se306.a1.scheduler.data.GraphParser;
+import se306.a1.scheduler.data.ParseException;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        GraphParser p = new GraphParser();
+        try {
+            p.parseGraph("input_graphs/Nodes_10_Random.dot");
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/se306/a1/scheduler/data/GraphParser.java
+++ src/main/java/se306/a1/scheduler/data/GraphParser.java
@@ -1,6 +1,22 @@
 package se306.a1.scheduler.data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * This class is responsible for parsing graphs in the .dot file format into
+ * TaskGraph objects and vice versa, from a TaskGraph to a valid .dot file.
+ *
+ * @author Abhinav Behal
+ */
 public class GraphParser {
+
+    private final static Pattern NAME_REGEX = Pattern.compile("\"([^\"]+)\"");
+    private final static Pattern NODE_REGEX = Pattern.compile("\\W*([\\w\\d]+)\\W*\\[Weight=(\\d+)");
+    private final static Pattern EDGE_REGEX = Pattern.compile("\\W*([\\w\\d]+)\\W*\\->\\W*([\\w\\d]+)\\W*\\[Weight=(\\d+)");
 
     /**
      * Parses the input .dot file into an TaskGraph object.
@@ -8,10 +24,39 @@ public class GraphParser {
      * into a data object with nodes, edges, and corresponding costs. Within
      * this hierarchy, parent and child relationships will explicit.
      *
-     * @param input filepath of input .dot file
+     * @param inputPath filepath of input .dot file
      * @return parsed object containing nodes, links, costs
+     * @throws IOException    if the file cannot be found, or if an IO error occurs while reading
+     * @throws ParseException if the graph cannot be parsed
      */
-    public TaskGraph parseInput(String input) {
+    public TaskGraph parseGraph(String inputPath) throws IOException, ParseException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputPath))) {
+            String name = "";
+            String line = reader.readLine();
+            Matcher nameMatcher = NAME_REGEX.matcher(line);
+            if (nameMatcher.find()) {
+                name = nameMatcher.group(1);
+            } else {
+                throw new ParseException(line);
+            }
+            System.out.println("Name: " + name);
+            while ((line = reader.readLine()) != null) {
+                Matcher nodeMatcher = NODE_REGEX.matcher(line);
+                Matcher edgeMatcher = EDGE_REGEX.matcher(line);
+                if (edgeMatcher.find()) {
+                    String first = edgeMatcher.group(1);
+                    String second = edgeMatcher.group(2);
+                    int weight = Integer.parseInt(edgeMatcher.group(3));
+                    System.out.println("Edge " + first + "->" + second + " with weight " + weight);
+                } else if (nodeMatcher.find()) {
+                    String label = nodeMatcher.group(1);
+                    int weight = Integer.parseInt(nodeMatcher.group(2));
+                    System.out.println("Node " + label + " with weight " + weight);
+                } else if (line.equals("}")) {
+                    break;
+                }
+            }
+        }
         return null;
     }
 
