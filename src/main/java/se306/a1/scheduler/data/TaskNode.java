a8e39e47d1a3f9aaa28e2c247b5a1c7ba0573191
Z-Qi <g.qiao97@gmail.com>
2018-07-26 21:14:22 +1200

    Implemented concrete TaskScheduleGraph and TaskNode classes

---
+++ src/main/java/se306/a1/scheduler/data/TaskNode.java
@@ -0,0 +1,15 @@
+package se306.a1.scheduler.data;
+
+/**
+ * Node representing a task object.
+ */
+public class TaskNode extends Node {
+
+    public TaskNode() {
+        super();
+    }
+
+    public TaskNode(String name, int value) {
+        super(name, value);
+    }
+}
---
+++ src/main/java/se306/a1/scheduler/data/TaskScheduleGraph.java
@@ -0,0 +1,46 @@
+package se306.a1.scheduler.data;
+
+import java.util.HashMap;
+
+/**
+ * The class used to represent a task schedule.
+ * @author Zhi Qiao
+ */
+public class TaskScheduleGraph implements TaskGraph {
+    private HashMap<String, Node> nodes;
+
+    public TaskScheduleGraph() {
+        nodes = new HashMap<>();
+        nodes.put("root", new TaskNode("root", 0));
+    }
+
+    @Override
+    public Node getRootNode() {
+        return nodes.get("root");
+    }
+
+    @Override
+    public boolean addNode(String name, int value) {
+        if (nodes.containsKey(name)) return false;
+        else {
+            nodes.put(name, new TaskNode(name, value));
+            return true;
+        }
+    }
+
+    @Override
+    public Node getNode(String name) {
+        return nodes.get(name);
+
+    }
+
+    @Override
+    public boolean addEdge(String parentName, String childName, int cost) {
+        if (nodes.containsKey(parentName) && nodes.containsKey(childName)) {
+            nodes.get(parentName).addChild(nodes.get(childName));
+            nodes.get(parentName).addLink(cost);
+            return true;
+        } else
+            return false;
+    }
+}
