0046b856eb00c78912408e8eee3c59262c8ca378
Z-Qi <g.qiao97@gmail.com>
2018-07-26 20:09:46 +1200

    Added TaskGraph interface

--- src/main/java/se306/a1/scheduler/data/TaskGraph.java
+++ src/main/java/se306/a1/scheduler/data/TaskGraph.java
@@ -1,4 +1,34 @@
 package se306.a1.scheduler.data;
 
+/**
+ * Specifies the interface for all graphs representing a task schedule.
+ * Classes implementing this interface should store the complete task schedule
+ * and provide the implementation for the defined contracts. 
+ * @author Zhi Qiao
+ */
 public interface TaskGraph {
+
+    /**
+     * Gets the root node task of the task schedule graph.
+     * @return the root node of the task schedule
+     */
+    public Node getRootNode();
+    
+    /**
+     * Adds a Node object to the task schedule graph.
+     * @return if the node was added successfully
+     */
+    public boolean addNode(Node node);
+
+    /**
+     * Gets the Node object represented by its name.
+     * @return Node object represented by its string value
+     */
+    public Node getNode(String name);
+
+    /**
+     * Adds an edge between two nodes, both nodes must be in the graph already.
+     * @return if the edge was added successfully
+     */
+    public boolean addEdge(Node parentNode, Node childNode, int cost);
 }
