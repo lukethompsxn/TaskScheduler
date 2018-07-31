3470c1ab3ea15d6fbfcbef28ea6baae1cb108f9e
Abhinav Behal <abhinav.behal@hotmail.com>
2018-07-31 23:51:33 +1200

    Refactored the processor class

--- src/main/java/se306/a1/scheduler/data/Processor.java
+++ src/main/java/se306/a1/scheduler/data/Processor.java
@@ -1,46 +1,65 @@
 package se306.a1.scheduler.data;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
- * This class represents a processor, including it's previous tasks and the time at which the previous task
- * on the processor will finish.
- * @author Rodger Gu
- *  */
+ * This class represents a processor and manages the tasks allocated to it.
+ *
+ * @author Rodger Gu, Abhinav Behal
+ */
 public class Processor {
-	private HashMap<Integer, Node> previousTasks;
-	private List<Integer> times;
-	private int time = 0;
+    private final Map<Node, Integer> startTimes;
+    private final String name;
+    private int earliestStartTime;
 
-	protected Processor() {
-		previousTasks = new HashMap<>();
-		times = new ArrayList<>();
-	}
-	
-	public void process(Node n, int start) {
-		times.add(start);
-		previousTasks.put(start, n);
-		time = start + n.getCost();
-	}
-	
-	public Collection<Node> getTasks() {
-		return previousTasks.values();
-	}
-	
-	public Collection<Integer> getTimes() {
-		return times;
-	}
-	
-	public Node getNode(int time) {
-		return previousTasks.get(time);
-	}
-	
-	public int getTime() {
-		return time;
-	}
+    /**
+     * Creates a new processor with a given name.
+     *
+     * @param name the name of the processor
+     */
+    Processor(String name) {
+        startTimes = new HashMap<>();
+        this.name = name;
+        this.earliestStartTime = 0;
+    }
+
+    /**
+     * Schedules a task (node) to be run on the processor.
+     *
+     * @param node the task to be scheduled
+     * @param time the time at which to schedule the task
+     */
+    void schedule(Node node, int time) {
+        if (!startTimes.containsKey(node))
+            throw new RuntimeException("Node " + node + " has already been scheduled");
+        else if (time < earliestStartTime)
+            throw new RuntimeException("Cannot schedule node " + node + " at time " + time);
+
+        startTimes.put(node, time);
+        earliestStartTime = node.getCost() + time;
+    }
+
+    /**
+     * Gets the scheduled start time of the given node.
+     *
+     * @param node the node to get the start time of
+     * @return the start time of the node
+     * @throws RuntimeException if the node has not been scheduled on this processor
+     */
+    public int getStartTime(Node node) {
+        if (!startTimes.containsKey(node))
+            throw new RuntimeException("Node " + node + " has not been scheduled");
+
+        return startTimes.get(node);
+    }
+
+    /**
+     * Gets the name of this processor.
+     *
+     * @return the name of this processor
+     */
+    public String getName() {
+        return name;
+    }
 }
