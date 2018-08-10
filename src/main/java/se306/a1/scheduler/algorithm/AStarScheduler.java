f69ed82ad61658e372208d75b53507a421d1c0ed
lukethompson <lukethompsxn@gmail.com>
2018-08-10 18:14:27 +1200

    Implemented the getCost() method in AStar Scheduler to determine the cost of scheduling a node on a specific processor

--- src/main/java/se306/a1/scheduler/algorithm/AStarScheduler.java
+++ src/main/java/se306/a1/scheduler/algorithm/AStarScheduler.java
@@ -1,5 +1,8 @@
 package se306.a1.scheduler.algorithm;
 
+import se306.a1.scheduler.data.graph.Node;
+import se306.a1.scheduler.data.schedule.Processor;
+import se306.a1.scheduler.data.schedule.Schedule;
 import se306.a1.scheduler.util.exception.ScheduleException;
 
 public class AStarScheduler extends Scheduler {
@@ -7,5 +10,28 @@ public class AStarScheduler extends Scheduler {
     @Override
     protected void createSchedule() throws ScheduleException {
 
+    }
+
+    /**
+     * This method computes the cost of scheduling a task on a processor based
+     * on its parents and the processor to be scheduled on. The cost returned
+     * is the total time after carrying out task.
+     *
+     * @param node the node we are wanting to schedule
+     * @param schedule the current schedule state we are on
+     * @param processor the processor to schedule to the task on
+     * @return the time after executing the task
+     * @throws ScheduleException
+     */
+    private int getCost(Node node, Schedule schedule, Processor processor) throws ScheduleException {
+        int cost = 0;
+
+        for (Node parent : graph.getParents(node)) {
+            cost = Math.max(cost, processor.getEarliestStartTime() + node.getCost());
+            if (!schedule.getProcessor(parent).equals(processor)) {
+                cost = Math.max(cost, schedule.getStartTime(parent) + parent.getCost() + graph.getCost(parent, node) + node.getCost());
+            }
+        }
+        return cost;
     }
 }
