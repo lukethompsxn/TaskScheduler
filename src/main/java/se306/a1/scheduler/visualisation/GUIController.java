3ea8231f5e95ebede4d7401ab6954d170da07daf
joelz1 <joel.clarke@live.com>
2018-08-15 18:17:00 +1200

    Added GUIController and GUILauncher for running javaFX GUI.

---
+++ src/main/java/se306/a1/scheduler/visualisation/GUIController.java
@@ -0,0 +1,78 @@
+package se306.a1.scheduler.visualisation;
+
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import se306.a1.scheduler.Main;
+import se306.a1.scheduler.algorithm.AStarByteScheduler;
+import se306.a1.scheduler.data.schedule.Schedule;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class GUIController implements Initializable {
+
+    @FXML
+    private Label timeLabel;
+
+    @FXML
+    private Label bestLabel;
+
+    @FXML
+    private Label threadsLabel;
+
+    @FXML
+    private Label statesLabel;
+
+    @FXML
+    private Label queueLabel;
+
+    @FXML
+    private Label nodesLabel;
+
+    @FXML
+    private Label edgesLabel;
+
+
+    public GUIController() {}
+    public GUIController(GUILauncher launcher) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // start running the algorithm
+        Task task = new Task<Void>() {
+            @Override
+            public Void call() {
+                //Main.run();
+                return null;
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
+    }
+
+    public void updateStats(Map<String, String> stats) {
+        for (String stat : stats.keySet()) {
+            switch (stat) {
+                case "NODES": nodesLabel.setText(stats.get(stat));
+                    break;
+                case "EDGES": edgesLabel.setText(stats.get(stat));
+                    break;
+                case "THREADS": threadsLabel.setText(stats.get(stat));
+                    break;
+                case "QUEUE LENGTH": queueLabel.setText(stats.get(stat));
+                    break;
+                case "STATES SEEN": statesLabel.setText(stats.get(stat));
+                    break;
+                case "RUN TIME": timeLabel.setText(stats.get(stat));
+                    break;
+
+            }
+        }
+    }
+}
---
+++ src/main/java/se306/a1/scheduler/visualisation/GUILauncher.java
@@ -0,0 +1,48 @@
+package se306.a1.scheduler.visualisation;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class GUILauncher extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootPane;
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Task Scheduler");
+        try {
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/Visualiser.fxml"));
+
+            GUIController controller = new GUIController(this);
+            loader.setController(controller);
+
+            rootPane = loader.load();
+
+            Scene scene = new Scene(rootPane, 1080, 720);
+            primaryStage.setScene(scene);
+
+            //exit program when closing window
+            //primaryStage.setOnCloseRequest(event -> System.exit(0));
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
