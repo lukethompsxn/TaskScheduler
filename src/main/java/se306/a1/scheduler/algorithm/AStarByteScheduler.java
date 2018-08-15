aa3efbcaef8d0e3a7d1f7f9b927f744999cfb7dd
Rodger Gu <RodgerG@hotmail.co.nz>
2018-08-15 23:14:46 +1200

    removed redundant if/else check

--- src/main/java/se306/a1/scheduler/algorithm/AStarByteScheduler.java
+++ src/main/java/se306/a1/scheduler/algorithm/AStarByteScheduler.java
@@ -13,48 +13,44 @@ import java.util.concurrent.TimeUnit;
 
 public class AStarByteScheduler extends Scheduler {
 
-    @Override
-    protected void createSchedule() throws ScheduleException {
-        ByteStateManager stateManager = new ByteStateManager(graph, processors, isVisualised);
-        // Adds a new state for each entry node on a processor
-        for (Node node : graph.getEntryNodes()) {
-            ByteState s = new ByteState(stateManager, graph);
-            stateManager.queue(s.scheduleTask(node, stateManager.getProcessor(0)));
-        }
+	@Override
+	protected void createSchedule() throws ScheduleException {
+		ByteStateManager stateManager = new ByteStateManager(graph, processors, isVisualised);
+		// Adds a new state for each entry node on a processor
+		for (Node node : graph.getEntryNodes()) {
+			ByteState s = new ByteState(stateManager, graph);
+			stateManager.queue(s.scheduleTask(node, stateManager.getProcessor(0)));
+		}
 
-        if (cores > 1) {
-            ExecutorService executor = Executors.newFixedThreadPool(cores);
-            for (int i = 0; i < cores; ++i) {
-                executor.execute(() -> runSequential(stateManager));
-            }
-            executor.shutdown();
-            try {
-                // wait for all threads to finish
-                executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        } else {
-            runSequential(stateManager);
-        }
+		ExecutorService executor = Executors.newFixedThreadPool(cores);
+		for (int i = 0; i < cores; ++i) {
+			executor.execute(() -> runSequential(stateManager));
+		}
+		executor.shutdown();
+		try {
+			// wait for all threads to finish
+			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
-        schedule = stateManager.getOptimal().toSchedule();
-    }
+		schedule = stateManager.getOptimal().toSchedule();
+	}
 
-    private void runSequential(ByteStateManager manager) {
-        while (!manager.hasOptimal() && !Thread.currentThread().isInterrupted()) {
-            ByteState current = manager.dequeue();
-            if (current == null)
-                continue;
-            current.order();
-            List<Node> freeNodes = current.getFreeNodes();
+	private void runSequential(ByteStateManager manager) {
+		while (!manager.hasOptimal() && !Thread.currentThread().isInterrupted()) {
+			ByteState current = manager.dequeue();
+			if (current == null)
+				continue;
+			current.order();
+			List<Node> freeNodes = current.getFreeNodes();
 
-            for (Node node : freeNodes) {
-                for (Processor processor : manager.getProcessors()) {
-                    ByteState next = current.scheduleTask(node, processor);
-                    manager.queue(next);
-                }
-            }
-        }
-    }
+			for (Node node : freeNodes) {
+				for (Processor processor : manager.getProcessors()) {
+					ByteState next = current.scheduleTask(node, processor);
+					manager.queue(next);
+				}
+			}
+		}
+	}
 }
